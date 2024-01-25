fdad073bd44107b6d8713d7dc8d33c325a2fa26d
Matt A. Tobin <email@mattatobin.com>
2024-01-24 21:06:45 -0600

    Change to an Example Application

--- confvars.sh
+++ confvars.sh
@@ -4,16 +4,14 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 MOZ_APP_VENDOR=BinOC
-#MOZ_APP_BASENAME=TestApp
-#MOZ_APP_NAME=testapp
-MOZ_APP_VERSION=`cat ${_topsrcdir}/testapp/config/version.txt`
+MOZ_APP_VERSION=`cat ${_topsrcdir}/xreapps/example/config/version.txt`
 MOZ_APP_VERSION_DISPLAY=${MOZ_APP_VERSION}
-BROWSER_CHROME_URL=chrome://testapp/content/testapp.xhtml
+
 
 # MOZ_APP_DISPLAYNAME will be set by branding/configure.sh
 # MOZ_BRANDING_DIRECTORY is the default branding directory used when none is
 # specified.
-MOZ_BRANDING_DIRECTORY=testapp/branding/unofficial
-#MOZ_OFFICIAL_BRANDING_DIRECTORY=testapp/branding/official
+MOZ_BRANDING_DIRECTORY=xreapps/example/branding/unofficial
+#MOZ_OFFICIAL_BRANDING_DIRECTORY=xreapps/example/branding/official
 MOZ_APP_ID={c819f695-b35f-49d1-8b97-957ad5f8ea47}
 
